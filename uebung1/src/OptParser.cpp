9bdbe8043b50fe0612a95996464ec88962af0337
Cellebyte <marcel.fest@live.de>
2017-03-13 14:02:18 +0100

    Fixed Parser to match the exercise

--- uebung1/src/OptParser.cpp
+++ uebung1/src/OptParser.cpp
@@ -12,6 +12,7 @@ bool CmdLineOptParser::Parse(int argc, char* argv[])
     for (int i=1; i<argc;i++)
     {
         char* argument=argv[i];
+        if(argument=='\0') return true;
         if(argument[0]!='-') return false;
         if(argument[1]=='\0') return false;
         char option=argv[i][1];
@@ -39,14 +40,19 @@ bool CmdLineOptParser::Parse(int argc, char* argv[])
         }
         else
         {
-            i++;
-            if(! Option(option,argv[i])) return false;
+            if(argv[i+1])
+            {
+                if(argv[i+1][0]!='-')
+                {
+                    if(! Option(option,argv[i+1])) return false;
+                    i++;
+                }
+            }
         }
     }
     return true;
 }
-bool CmdLineOptParser::Option(const char c , const char* info)
+bool CmdLineOptParser::Option(const char , const char* )
 {
-    if(c!='\0' and info!='\0') return true;
-	else return false;
+    return true;
 }
