8a6290a22d615043cdfa83fd5ee36ccee52e8ecd
Cellebyte <marcel.fest@live.de>
2017-03-15 16:59:50 +0100

    Addet somehow working version. Output is not as expected

--- uebung1/src/Printf.cpp
+++ uebung1/src/Printf.cpp
@@ -5,7 +5,7 @@
  */
 
 #include <cstdarg>
-#include <cstring>
+#include <cstdio>
 #include "Printf.h"
 /*
  *  @function Printf
@@ -32,9 +32,12 @@ char* Printf( char* dst, const void* end, const char* fmt, ... )
     char temp=0;                    // define buffer character
     char* iter=dst;                 // define iterator over array;
     char* erg=0;                    // result of switch case
-    while(fmt!='\0')
+    unsigned int val=0;
+    int value=0;
+    while(*fmt!='\0')
     {
-        if((void*)iter>=end)
+        // DEBUG printf("%c",*fmt);
+        if(iter>=end)
         {
             /*  #TODO fix Bufferoverflow.
              * No correct solution at the moment
@@ -42,93 +45,79 @@ char* Printf( char* dst, const void* end, const char* fmt, ... )
 
             return '\0';
         }
-        temp=*(fmt);
-
+        temp=*fmt;
         if (temp!='%')      //  if not a format copy char to buffer
         {
-             *iter=temp;
+            *iter++=temp;
         }
         else
         {
             fmt++;
-            temp=*(fmt);
+            temp=*fmt;
+            // DEBUG printf("%c",*fmt);
             switch(temp)    //  else switch on format string
             {
                 case 'd':
+                    value=va_arg(vl,int);
+                    if (value<0)  //  check if integer is negative
                     {
-                        int val=va_arg(vl,int);
-                        if (val<0)  //  check if integer is negative
-                        {
-                            *iter++='-';
-                        }
-                        while(val)  //  use modulo for convert to string
-                        {
-                            *iter++=val%10+'0';
-                            val=val/10;
-                        }
-                        break;
+                        *iter++='-';
                     }
+                    while(value)  //  use modulo for convert to string
+                    {
+                        *iter++=value%10+'0';
+                        val=val/10;
+                    }
+                    break;
                 case 'u':
+                    val=va_arg(vl,unsigned int);
+                    while(val)
                     {
-                        unsigned int val=va_arg(vl,unsigned int);
-                        while(val)
-                        {
-                            *iter++=val%10+'0';
-                            val=val/10;
-                        }
-                        break;
+                        *iter++=val%10+'0';
+                        val=val/10;
                     }
+                    break;
                 case 'c':
-                    {
-                        *iter++=va_arg(vl,int);
-                        break;
-                    }
+                    *iter++=va_arg(vl,int);
+                    break;
                 case 's':
+                    erg=va_arg(vl,char*);
+                    while(*erg!='\0')
                     {
-                        erg=va_arg(vl,char*);
-                        while(erg!='\0')
-                        {
-                            *iter++=*erg++;
-                        }
-                        break;
+                        *iter++=*erg++;
                     }
+                    break;
                 case 'x':
+                    val=va_arg(vl,unsigned int);
+                    *iter++='0';
+                    *iter++='x';
+                    while(val)
                     {
-                        unsigned int val=va_arg(vl,unsigned int);
-                        *iter++='0';
-                        *iter++='x';
-                        while(val)
-                        {
-                            *iter++=val%16+'0';
-                            val=val/16;
-                        }
-                        break;
+                        *iter++=val%16+'0';
+                        val=val/16;
                     }
+                     break;
 
                 case 'b':
-                    {
-                        unsigned int val=va_arg(vl,unsigned int);
-                        *iter++='0';
-                        *iter++='b';
-                        while(val)
-                        {
-                            *iter++=val%2+'0';
-                            val=val/2;
-                        }
-                        break;
-                    }
+                    val=va_arg(vl,unsigned int);
+                    *iter++='0';
+                    *iter++='b';
+                    do{
+                        *iter++=val%2+'0';
+                        val=val/2;
+                    }while(val);
+                    break;
                 case '%':
-                    {
-                        *iter++=temp;
-                        break;
-                    }
-                default:break;
+                    *iter++=temp;
+                    break;
+                default:
+                    return '\0';
             }
         }
         fmt++;
     }
-    iter='\0';
     va_end(vl);
+    iter='\0';
     return dst;
 }
 
--- uebung1/src/main.cpp
+++ uebung1/src/main.cpp
@@ -5,10 +5,14 @@
 #include "OptParser.h"
 #include "Printf.h"
 #include <cstdio>
-int main(int argc, char* argv[])
+//int main(int argc, char* argv[])
+int main()
 {
-    CmdLineOptParser cmd;
-    cmd.Parse(--argc,++argv);
+    //CmdLineOptParser cmd;
+    //cmd.Parse(--argc,++argv);
     char a[1000];
-    printf("%s",Printf(a,(void*)&a[1000],"%x, %d, %d, %s, %c, %b, %u, %%",16,-12345,12345," Hallo \0", 'a', 16, 1233));
+    unsigned int test=16;
+    //printf("%s",Printf(a,(void*)&a[1000],"%x, %d, %d, %s, %c, %b, %u, %%",16,-12345,12345," Hallo \0", 'a', 16, 1233))
+    char* result=Printf(a,a+999,"%b\n",test);
+    printf("%s",result);
 }
