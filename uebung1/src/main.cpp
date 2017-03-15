acef7b609e827654067ab836f4047769d02bba75
Cellebyte <marcel.fest@live.de>
2017-03-15 15:17:22 +0100

    try to test printf.cpp... Fails at execution

--- uebung1/src/main.cpp
+++ uebung1/src/main.cpp
@@ -4,9 +4,11 @@
 
 #include "OptParser.h"
 #include "Printf.h"
+#include <cstdio>
 int main(int argc, char* argv[])
 {
     CmdLineOptParser cmd;
-    if(cmd.Parse(--argc,++argv)) return 0;
-    else return 1;
+    cmd.Parse(--argc,++argv);
+    char a[1000];
+    printf("%s",Printf(a,(void*)&a[1000],"%x, %d, %d, %s, %c, %b, %u, %%",16,-12345,12345," Hallo \0", 'a', 16, 1233));
 }
