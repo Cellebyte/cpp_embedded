10feb618694732f536d7a7ef80f296ced8884ba3
Cellebyte <marcel.fest@live.de>
2017-03-19 17:18:25 +0100

    Fix unnecessary Wildcard

--- build.sh
+++ build.sh
@@ -3,25 +3,21 @@
 touch Makefile
 touch config.mk
 
-
-
 echo 'CXX=g++-6' > config.mk
 echo 'CXXFLAGS=-Werror -Wall -Wextra -fno-exceptions -Wno-format -fno-rtti -std=c++1y' >> config.mk
 echo 'OUTPUT="./bin"' >> config.mk
 echo 'SILENT_MKDIR=' >> config.mk
 
+
 echo 'include config.mk' > Makefile
 echo 'all: $(OUTPUT) build' >> Makefile
-
 echo 'build:' >> Makefile
 for i in $(ls -d */); do echo '	@$(CXX) $(CXXFLAGS) -o $(OUTPUT)/'"${i%%/}"' '"${i%%/}"'/src/*.cpp -I '"${i%%/}"'/include/' >> Makefile \
 && echo '	@echo'' 1. CC '"${i%%/}" >> Makefile \
 ; done
 
-
 echo 'start:' >> Makefile
 echo '	$(SILENT_MKDIR)mkdir $(OUTPUT)' >> Makefile
-
 echo 'clean:' >> Makefile
 echo '	rm -rf $(OUTPUT)' >> Makefile
 
@@ -29,5 +25,5 @@ make clean
 make start
 make build
 
-for i in $(ls bin/*); do ./$i; done
+for i in $(ls bin/); do ./$i; done
 
