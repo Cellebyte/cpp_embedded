d1803e78e92952aaebf565e5792c25be15eca56e
Cellebyte <marcel.fest@live.de>
2017-03-20 23:56:01 +0100

    Twist if statements and define some things for C Preprocessor

--- uebung2/src/Printf.cpp
+++ uebung2/src/Printf.cpp
@@ -7,6 +7,19 @@
 #include <cstdarg>
 //#include <cstdio>
 #include "Printf.h"
+
+#define END_OF_STRING       '\0'
+#define NEWLINE             '\n'
+#define NULL(x)             x=0
+
+#define INTEGER             'd'
+#define CHARACTER           'c'
+#define UNSIGNED_INTEGER 	'u'
+#define HEXADECIMAL         'x'
+#define BINARY              'b'
+#define STRING              's'
+#define PROCENT             '%'
+
 /*
  *  Defined Functions
  *  @function unsigned_int_to_number_system_string
@@ -16,7 +29,7 @@
  *  @return char* pointer on the last free element in buffer.
  */
 char* unsigned_int_to_number_system_string(char*, unsigned int, int);
-static const char dig_its[]="0123456789abcdef";
+static const char dig_its[] = "0123456789abcdef";
 
 /*
  *  @function Printf
@@ -37,90 +50,90 @@ char* Printf( char* dst, const void* end, const char* fmt, ... )
      *  6.  %b for binary representation (0b10101)
      *  7.  %% for %
      */
-    va_list vl;                     // variable arg list initialization
-    va_start(vl, fmt);              // define variable params after fmt
-    char temp=0;                    // define buffer character
-    char* iter=dst;                 // define iterator over array;
-    char* erg=0;                    // result of switch case
-    unsigned int val=0;             // value for 2. 5. 6.
-    int value=0;                    // value for 1.
-    while(*fmt!='\0' && iter<end) // - two for \n and \0 at the end of the loop
+    va_list vl;                         // variable arg list initialization
+    va_start(vl, fmt);                  // define variable params after fmt
+    char NULL(temp);                    // define buffer character
+    char* iter=dst;                     // define iterator over array
+    char* NULL(erg);                    // result of switch case
+    unsigned int NULL(val);             // value for 2. 5. 6.
+    int NULL(value);                    // value for 1.
+
+    while(END_OF_STRING != *fmt && iter < end) // - two for \n and \0 at the end of the loop
     {
         // #TODO Fix Bufferoverflow!!
-        temp=*fmt;
-        if (temp!='%')      //  if not a format string copy char to destination array
+        temp = *fmt;
+        if (PROCENT != temp)      //  if not a format string copy char to destination array
         {
-            *iter++=temp;
+            *iter++ = temp;
         }
         else
         {
-            fmt++;
-            temp=*fmt;
-            // DEBUG printf("%c",*fmt);
+            fmt++; //--> Next Character
+            temp = *fmt;
+            // #DEBUG printf("%c",*fmt);
             switch(temp)    // goto right format and add its formatted string to destination
             {
-                //  signed Integer
-                case 'd':
-                    value=va_arg(vl,int);   //  get element from stack
-                    if (value<0)  //  check if integer is negative
+                case INTEGER:
+                    value = va_arg(vl,int);   //  get element from stack
+                    if (0 > value)  //  check if integer is negative
                     {
-                        *iter++='-';
-                        value=value*-1;
+                        *iter++ = '-';
+                        value = value*-1;
                     }
-                    iter=unsigned_int_to_number_system_string(iter,(unsigned int)value,10); // use modulo to map right string
+                    iter = unsigned_int_to_number_system_string(iter,(unsigned int)value,10); // use modulo to map right string
                     break;
-                //  unsigned Integer
-                case 'u':
-                    val=va_arg(vl,unsigned int);
-                    iter=unsigned_int_to_number_system_string(iter,val,10); // use modulo to map right string
+
+                case UNSIGNED_INTEGER:
+                    val = va_arg(vl,unsigned int);
+                    iter = unsigned_int_to_number_system_string(iter,val,10); // use modulo to map right string
                     break;
-                //  character
-                case 'c':
-                    *iter++=va_arg(vl,int);
+
+                case CHARACTER:
+                    *iter++ = va_arg(vl,int);
                     break;
-                //  String --> char Array
-                case 's':
-                    erg=va_arg(vl,char*);
-                    while(*erg!='\0')       //append given string without \0 to destination array
+
+                case STRING://--> Char Array
+                    erg = va_arg(vl,char*);
+                    while(END_OF_STRING != *erg)       //append given string without \0 to destination array
                     {
-                        *iter++=*erg++;
+                        *iter++ = *erg++;
                     }
                     break;
-                //  unsigned Integer to hex presentation
-                case 'x':
-                    val=va_arg(vl,unsigned int);
-                    *iter++='0';
-                    *iter++='x';
-                    iter=unsigned_int_to_number_system_string(iter,val,16);
+
+                case HEXADECIMAL:
+                    val = va_arg(vl,unsigned int);
+                    *iter++ = '0';
+                    *iter++ = HEXADECIMAL;
+                    iter = unsigned_int_to_number_system_string(iter,val,16);
                     break;
-                //  unsigned Integer to byte presentation
-                case 'b':
+
+                case BINARY:
                     val=va_arg(vl,unsigned int);
-                    *iter++='0';
-                    *iter++='b';
-                    iter=unsigned_int_to_number_system_string(iter,val,2);
+                    *iter++ = '0';
+                    *iter++ = BINARY;
+                    iter = unsigned_int_to_number_system_string(iter,val,2);
                     break;
-                //  append a simple % to the String
-                case '%':
-                    *iter++=temp;
+
+                case PROCENT:
+                    *iter++ = temp;
                     break;
                 //  if not defined return an empty destination
                 default:
-                    return '\0';
+                    return END_OF_STRING;
             }
         }
-        fmt++; //   go to next format string character
+        fmt++; // --> next character
     }
     va_end(vl);     //  close the variable parameters
-    *iter++='\n';   //  append \n --> #TODO \r\n for Windows and \r for OSX
-    *iter='\0';     //  append end of String
+    *iter++ = NEWLINE;   //  append \n --> #TODO \r\n for Windows and \r for OSX
+    *iter = END_OF_STRING;     //  append end of String
     return dst;     //  return the created String
 }
-char* unsigned_int_to_number_system_string(char* buffer,unsigned int val, int type)
+char* unsigned_int_to_number_system_string(char* buffer,unsigned int value, int type)
 {
-    char digit=dig_its[val%type];       // map in right char in array with modulo function
-    val=val/type;                       // decrease value
-    if(val) buffer=unsigned_int_to_number_system_string(buffer,val, type); // recursion if value not zero
-    *buffer++=digit;    // add digit to string
-    return buffer;      // return pointer on free element in buffer
+    char digit = dig_its[value%type];       // map right char in array with modulo function
+    value = value/type;                     // decrease value divided by type
+    if(value) buffer = unsigned_int_to_number_system_string(buffer,value, type); // recursion if value not zero
+    *buffer++ = digit;    // add digit to string
+    return buffer;      // return pointer on free element after last element
 }
