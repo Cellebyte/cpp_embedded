19880f293e0ef0cb4c3c3d236fa0c2ab088fc74a
Cellebyte <marcel.fest@live.de>
2017-03-22 09:28:02 +0100

    Delete Unnecessary Code

--- uebung1/src/OptParser.cpp
+++ uebung1/src/OptParser.cpp
@@ -21,12 +21,6 @@ bool CmdLineOptParser::Parse(int argc, char* argv[])
     char* NULL(argument);
     char NULL(option);
 
-    /*
-     *  Only for move of String at line 45
-     */
-    int NULL(hop);
-    int NULL(begin);
-
     for (int i=1; i<argc; i++)
     {
         //parse if an option exist and store this option in an temporary variable
@@ -34,6 +28,7 @@ bool CmdLineOptParser::Parse(int argc, char* argv[])
         if(FLAG != argument[0]) return false;
         if(END_OF_STRING == argument[1]) return false;
         option = argument[1];
+
         /*  Parse different types of arguments
          *  x stands for an option
          *  1. -x
@@ -41,62 +36,46 @@ bool CmdLineOptParser::Parse(int argc, char* argv[])
          *  3. -x=string
          *  4. -x string
          */
+
         // check if variant 2. or 3. are given
         if(END_OF_STRING != argument[2])
         {
-            // counter variable of the given while loop in line 49
-            NULL(hop);
-            NULL(begin);
-            // check for variant 3 with '='
             if('=' == argument[2])
             {
-                hop=3;
-                begin=3;
+                    argument = &(argument[3]);
             }
             // now it is variant without the '='
             else
             {
-                hop=2;
-                begin=2;
+                argument = &(argument[2]);
             }
-            //move given string to the right Memory Addresses
-            while(END_OF_STRING != argument[hop])
-            {
-                // copy string from offset of 3 or 2 to the front
-                argument[hop-begin] = argument[hop];
-                hop++;
-            }
-            argument[hop-begin] = END_OF_STRING;
-            /*  end the String with \0
-             *  Check if The given Option and Argument is right
-             */
-            if(! Option(option,argument)) return false;
         }
         else
-        //check for variant 1. or 4.
+        // check if variant 1. or 4.
         {
             /*  lookahead for the next String in the given argument vector
              *  i is the Counter Variable from above
              */
+
             if(argv[i+1])
             {
                 // Variant 4 option and string are divided by a space
                 if(FLAG != argv[i+1][0]) //    if not an option Check use as argument an
                 {
                     argument = argv[++i]; // increase i for the next option
-                    if(! Option(option,argument)) return false;
                 }
                 else
                 {
-                    if(! Option(option,END_OF_STRING)) return false;
+                    argument = END_OF_STRING;
                 }
             }
             // no lookahead --> their is no string for the argument so only check the option
             else
             {
-                if(! Option(option,END_OF_STRING)) return false;
+                argument = END_OF_STRING;
             }
         }
+        if(! Option(option,argument)) return false;
     }
     // when the while loop parse everything
     return true;
