e0b93935b5f57a6038e4bb6d49a121f4c867d610
Cellebyte <marcel.fest@live.de>
2017-04-03 16:35:32 +0200

    Added test from @XOSplicer

--- uebung2/src/main.cpp
+++ uebung2/src/main.cpp
@@ -5,10 +5,33 @@
 #include "Printf.h"
 #include <cstdio>
 
-int main()
+/* int main()
 {
     char a[1000];
     unsigned int test=16;
     char* result=Printf(a+0,a+999,"%x, %d, %d, %s, %c, %b, %u, %% ",test,-12345,12345,"Hallo",'a',test,test);
     printf("%s\n",result);
+}*/
+int main() {
+    char buffer[256] = {0};
+    char small_buffer[32];
+    int ret = Printf(
+            buffer,
+            buffer+256,
+            "verbatim text %d, %u, %c, %s, %x, %b, %%",
+            -4316, 2958, '&', "some string", 0xdeadbeef, 0xdeadbeef
+            )? 0 : 1;
+    printf("%s",buffer);
+    ret |= Printf(
+            small_buffer, small_buffer+32,
+            "%%d%%%d",
+            -123
+            )? 0 : 1;
+    ret |= Printf(
+            small_buffer, small_buffer+32,
+            "%s",
+            "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            )? 1 : 0;
+    return ret;
 }
+
