d661ea71460789aae2878c555f373c6b3757fe47
Cellebyte <marcel.fest@live.de>
2017-04-04 09:20:12 +0200

    Fixed non returning of error code in build.sh --> @desyon Thank you

--- build.sh
+++ build.sh
@@ -1,4 +1,20 @@
 #! /usr/bin/env bash
+#   author: Cellebyte alias Marcel Fest
+# --> Fixed issue mentioned by @desyon
+# ----> Script is returning Error Code <----
+# ------> local build and test is also possible <------
+
+
+# For building and testing the whole folder:
+# -> INSTALL
+#   sudo apt-get install build-essential software-properties-common -y && \
+#   sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y && \
+#   sudo apt-get update && \
+#   sudo apt-get install gcc-6 g++-6 -y && \
+#   gcc -v
+#
+# -> Run ./build.sh
+
 
 touch Makefile
 touch config.mk
