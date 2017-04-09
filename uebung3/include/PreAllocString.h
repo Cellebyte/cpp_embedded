bf190fba5bb93e447d3e6c3e4c334ae87a99c852
Cellebyte <marcel.fest@live.de>
2017-04-09 19:03:10 +0200

    Added some comments in code

--- uebung3/include/PreAllocString.h
+++ uebung3/include/PreAllocString.h
@@ -9,6 +9,19 @@
 template <size_t MAX_LENGTH>
 class PreAllocString
 {
+    private:
+        size_t used_length;
+        const size_t max_length = MAX_LENGTH;
+        char string_storage[MAX_LENGTH];
+        const char* GetFrontPointer() const
+        {
+            return &string_storage[0];
+        }
+        const char& GetOnPosition(const int idx) const
+        {
+            return string_storage[idx];
+        }
+
     public:
         PreAllocString()
         {
@@ -36,6 +49,10 @@ class PreAllocString
             }
             used_length = 0;
         }
+        /*  uses Printf to append or add different Formats to string_storage
+         *  for reference which types are supported
+         *  view into the implementation of Printf under Printf.cpp
+         */
         void AddFormat(const char* format, ...)
         {
             va_list vl;
@@ -45,11 +62,49 @@ class PreAllocString
             used_length = temp - string_storage;
             va_end(vl);
         }
-
+        /* Adds one Space into the next Writable Character and increments*/
         void AddWhiteSpace()
         {
             string_storage[used_length++]=' ';
         }
+
+        /*  Overloaded operators:
+         *  1. '='      ->> varName = "String"
+         *  2. '='      ->> varName = 'char'
+         *  3. '+='     ->> varName += "String" ⁼   varName.append("String")
+         *  4. '+='     ->> varName += 'char'   ⁼   varName.append('char')
+         *  5. '[]'     ->> varName[index] (only readable) ->> returns char
+         *  6. 'cast'   ->> cast to 'const void*' and cast to 'const char*'
+         */
+        PreAllocString& operator=( const char* rhs)
+        {
+            Empty();
+            AddFormat("%s", rhs);
+            return *this;
+        }
+        PreAllocString& operator=( char rhs )
+        {
+            Empty();
+            AddFormat("%c", rhs);
+            return *this;
+        }
+        PreAllocString& operator=( char* const rhs)
+        {
+            Empty();
+            AddFormat("%s", rhs);
+            return *this;
+        }
+        PreAllocString& operator+=( char const* rhs)
+        {
+            AddFormat("%s", rhs);
+            return *this;
+        }
+        PreAllocString& operator+=( char rhs)
+        {
+            AddFormat("%c", rhs);
+            return *this;
+        }
+        /*  Defined casts for this class*/
         operator const char *() const
         {
             return GetFrontPointer();
@@ -58,42 +113,11 @@ class PreAllocString
         {
             return static_cast<void> (GetFrontPointer());
         }
-        const char& operator[](const int idx);
-
-        PreAllocString& operator=( char rhs )
+        /*  Array [index] implementation */
+        const char& operator[](const int idx)
         {
-            Empty();
-            AddFormat("%c", rhs);
-            return *this;
+            return GetOnPosition(idx);
         }
-        PreAllocString& operator=( const char* rhs)
-        {
-            Empty();
-            AddFormat("%s", rhs);
-            return *this;
-        }
-        PreAllocString& operator=( char* const rhs)
-        {
-            Empty();
-            AddFormat("%s", rhs);
-        }
-        PreAllocString& operator+=( char rhs)
-        {
-            AddFormat("%c", rhs);
-        }
-        PreAllocString& operator+=( char const* rhs)
-        {
-            AddFormat("%s", rhs);
-        }
-    private:
-        size_t used_length;
-        const size_t max_length = MAX_LENGTH;
-        char string_storage[MAX_LENGTH];
-        const char* GetFrontPointer() const
-        {
-            return &string_storage[0];
-        }
-
 };
 
 #endif
