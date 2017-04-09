a3b076dbfe329537d5a5951b3de5cd84a371f1c3
Cellebyte <marcel.fest@live.de>
2017-04-09 17:41:34 +0200

    Addet simple Test

--- uebung3/src/main.cpp
+++ uebung3/src/main.cpp
@@ -1,1 +1,10 @@
 //This is the main
+
+#include "PreAllocString.h"
+#include <cstdio>
+int main()
+{
+    CREATE(test_string,100);
+    test_string = "asdf";
+    printf("%s\n",(const char*)test_string);
+}
