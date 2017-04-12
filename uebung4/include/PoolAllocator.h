d08a8ec14308a249e2ac8dea80bff2809e84a3b9
Cellebyte <marcel.fest@live.de>
2017-04-12 19:46:53 +0200

    @XOSplicer is this all I need?

--- uebung4/include/PoolAllocator.h
+++ uebung4/include/PoolAllocator.h
@@ -17,13 +17,25 @@ class IHeap
         virtual void Deallocate ( void *)= 0;
         /* Returns remaining # of available bytes */
         virtual size_t Available () const = 0;
+    private:
+        void* newBlock;
+        size_t size;
 };
 
 template<size_t blockCount>
 void* IHeap<blockCount>::Allocate(size_t sizeInBytes)
 {
+    size = sizeInBytes;
     return sizeof(sizeInBytes) newBlock [blockCount];
 }
-
+template<size_t blockCount>
+void IHeap<blockCount>::Deallocate()
+{
+    delete(newBlock);
+}
+size_t IHeap<blockCount>::Available() const
+{
+    return blockCount*size;
+}
 
 #endif
