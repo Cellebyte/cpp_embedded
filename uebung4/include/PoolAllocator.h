2f9cfd6efd98f612995792a7b3459c51e77546cc
Cellebyte <marcel.fest@live.de>
2017-04-13 18:05:40 +0200

    Addet things to allocate memory

--- uebung4/include/PoolAllocator.h
+++ uebung4/include/PoolAllocator.h
@@ -5,34 +5,43 @@
 #define POOLALLOCATOR_H
 
 #include <cstddef>
+#include <cstdint>
+#include <cstdlib>
 
-# define CREATE(varName, blockCount, blockSize) \
-                IHeap<blockCount>::Allocate(blockSize) varName;
+# define CREATE(varName, blockCount, blockSize) IHeap<blockCount> varName = IHeap<blockCount>::Allocate(blockSize);
 
-template <typename T, size_t blockCount>
+template <size_t blockCount>
 class IHeap
 {
     public:
-        virtual void * Allocate ( size_t sizeInBytes ) = 0;
-        virtual void Deallocate ( void *)= 0;
+        virtual void * Allocate (size_t sizeInBytes) = 0;
+        virtual void Deallocate (void *)= 0;
         /* Returns remaining # of available bytes */
         virtual size_t Available () const = 0;
     private:
-        void* newBlock;
         size_t size;
+        void* block_allocated[blockCount];
 };
 
 template<size_t blockCount>
 void* IHeap<blockCount>::Allocate(size_t sizeInBytes)
 {
     size = sizeInBytes;
-    return sizeof(sizeInBytes) newBlock [blockCount];
+    for(size_t i = 0; i < blockCount; i++)
+    {
+        block_allocated[i]=alloca(sizeof(int8_t)*sizeInBytes);
+    }
+    return block_allocated;
 }
 template<size_t blockCount>
-void IHeap<blockCount>::Deallocate()
+void IHeap<blockCount>::Deallocate(void* block_allocated)
 {
-    delete(newBlock);
+    for(size_t i = 0; i< blockCount; i++)
+    {
+        free(block_allocated);
+    }
 }
+template<size_t blockCount>
 size_t IHeap<blockCount>::Available() const
 {
     return blockCount*size;
--- uebung4/src/main.cpp
+++ uebung4/src/main.cpp
@@ -1,3 +1,11 @@
 /*
  *  @author Marcel Fest
  */
+
+#include "PoolAllocator.h"
+#include <cstdio>
+int main()
+{
+    CREATE(test,15,20);
+    printf("%d",static_cast<int>(test.Available()));
+}
