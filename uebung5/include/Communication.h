15f2b20563e16a0dc0f0a929f2c1d9a4bde6ad65
Cellebyte <marcel.fest@live.de>
2017-05-15 16:01:25 +0200

    use hotfix

--- uebung5/include/Communication.h
+++ uebung5/include/Communication.h
@@ -1,17 +1,9 @@
 /*
  *  @author Marcel Fest
  */
+
 #ifndef COMMUNICATION_H
 #define COMMUNICATION_H
-#include <stdlib.h>
-#include <string.h>
-#include <netdb.h>
-#include <stdio.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include "PoolAllocator.h"
-#include "PreAllocString.h"
 
 class ClientServer
 {
@@ -22,7 +14,7 @@ class ClientServer
             Server,
             Client
         };
-        bool Start ( Mode mode , unsigned int port , const char* ip );
+        bool Start (Mode mode , unsigned int port , const char* ip );
 };
 
 
